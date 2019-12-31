40cedb709a220c63b26a887c029af421c17fc32f
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-31 14:46:19 -0700

    finished search

--- dbview.php
+++ dbview.php
@@ -13,6 +13,59 @@ require_once("include/db.php");
     <head>
     <body>
         <h2 class="success"><?php echo @$_GET["id"]; ?></h2>
+        <div class="">
+            <fieldset>
+                <form class="" action="dbview.php" method="GET">
+                    <input type="text" name="Search" value="" placeholder="Search by name / ssn">
+                    <input type="submit" name="SearchButton" value="Search record">
+                </form>
+            </fieldset>
+        </div>
+        <?php 
+            if(isset($_GET["SearchButton"])) {
+                global $ConnectingDB;
+                $Search = $_GET["Search"];
+                $sql = "SELECT * FROM emp_record WHERE ename=:searcH OR ssn=:searcH";
+                $stmt=$ConnectingDB->prepare($sql);
+                $stmt->bindValue(':searcH', $Search);
+                $stmt->execute();
+                while($DataRows = $stmt->fetch()){
+                    $Id = $DataRows["id"];
+                    $EName = $DataRows["ename"];
+                    $SSN = $DataRows["ssn"];
+                    $Department = $DataRows["dept"];
+                    $Salary = $DataRows["salary"];
+                    $HomeAddress = $DataRows["homeaddress"];
+                ?>
+                <div>
+
+                <table width="1000" border="5" align="center">
+                   <caption>Search Result</caption> 
+                   <tr>
+                       <th>ID</th>
+                       <th>Name</th>
+                       <th>SSN</th>
+                       <th>Department</th>
+                       <th>Salary</th>
+                       <th>Home Address</th>
+                       <th>Search Again</th>
+                   </tr>
+                   <tr>
+                       <td><?php echo $Id; ?></td>
+                       <td><?php echo $EName; ?></td>
+                       <td><?php echo $SSN; ?></td>
+                       <td><?php echo $Department; ?></td>
+                       <td><?php echo $Salary; ?></td>
+                       <td><?php echo $HomeAddress; ?></td>
+                       <td><a href="dbview.php">Search Again</a></td>
+                   </tr>
+                </table>
+                </div>
+                
+            <?php }
+            }
+        
+        ?>
         <table width="1000" border="5" align="center">
             <caption>View From Database</caption>
             <tr>
