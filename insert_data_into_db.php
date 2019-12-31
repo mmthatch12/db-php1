0eb15a47f38837115edc314427757ef065b0e5c2
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-31 09:54:55 -0700

    created insert data file

---
+++ insert_data_into_db.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+
+<html>
+    <head>
+        <title>Insert Data into Database</title>
+    <head>
+    <body>
+        <div class="">
+            <form action="insert_data_into_db.php" method="post">
+                <fieldset>
+                    <span>Employee Name:</span>
+                    <br>
+                    <input type="text" name="EName" value="">
+                    <br>
+                </fieldset>
+
+            </form>
+
+        </div>
+
+    </body>
+</html>
