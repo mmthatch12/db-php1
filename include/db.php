4c8591de978249c9fafffe63850f11bedd5bd29d
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-31 10:29:45 -0700

    finished db connection file

---
+++ include/db.php
@@ -0,0 +1,6 @@
+<?php
+//                   data source network,default username, default password
+$DSN = 'mysql:host=localhost;dbname=record';
+$ConnectingDB = new PDO($DSN, 'root', '');
+
+?>
--- insert_data_into_db.php
+++ insert_data_into_db.php
@@ -3,15 +3,33 @@
 <html>
     <head>
         <title>Insert Data into Database</title>
+        <link rel="stylesheet" href="include/style.css">
     <head>
     <body>
-        <div class="">
+        <div class="formdiv">
             <form action="insert_data_into_db.php" method="post">
                 <fieldset>
-                    <span>Employee Name:</span>
+                    <span class="fieldinfo">Employee Name:</span>
                     <br>
                     <input type="text" name="EName" value="">
                     <br>
+                    <span class="fieldinfo">Social Security Number:</span>
+                    <br>
+                    <input type="text" name="SSN" value="">
+                    <br>
+                    <span class="fieldinfo">Department:</span>
+                    <br>
+                    <input type="text" name="Dept" value="">
+                    <br>
+                    <span class="fieldinfo">Salary:</span>
+                    <br>
+                    <input type="text" name="Salary" value="">
+                    <br>
+                    <span class="fieldinfo">Home Address:</span>
+                    <br>
+                    <textarea name="HomeAddress" rows="8" cols="80"></textarea>
+                    <br>
+                    <input type="submit" name="Submit" value="Submit your record">
                 </fieldset>
 
             </form>
